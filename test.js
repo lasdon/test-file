9103f4a250d1ee8fb90eca88116af0d93b96edd0
lasdon <s_t_a_k_you@yahoo.co.jp>
2020-10-08 20:37:07 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,11 @@
-document.write("<br>\n");
+document.write("<br>")
 let a=5
-document.write("a="+a+"<br>\n");
+document.write("a="+a+"<br>")
 for (let i = 0 ; i < 5 ; i++){
-  	document.write(i);
+  	document.write(i)
   }
-  document.write("<br>\n");
-
+document.write("<br>")
+document.write("配列の定義を検証する<br>")
+let b=[1,2,3]
+document.write("b[2]="+b[2])
+document.write("<br>")

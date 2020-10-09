1e58244cdc9978adf1e894730620bb286d7f146f
lasdon <s_t_a_k_you@yahoo.co.jp>
2020-10-09 15:01:24 +0900

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,9 +1,12 @@
-document.write("読み込み用ファイル2つ目<br>");
-int b[];
-b=new int[5];
-for(let i =0;i<5;i++){
-b[i]=i*i*i;
-if(b[i]>9){
-document.write(i+"番目"+"b="+b[i]"<br>");
+document.write("読み込み用ファイル2つ目(forn文とif文の確認)<br>");
+let d = new Array(5);
+for(let l =0;l<5;l++){
+    d[l]=l*l*l;
+    if(d[l]>9){
+        document.write(l+"番目"+"d="+d[l]+"<br>");
+    }else{
+        document.write(l+"番目"+"<br>");
+    }
+    
 }
-}
+
